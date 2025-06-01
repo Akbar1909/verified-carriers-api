ca8a4f72fa8d5be7e8dc27519c3c3fd24b591df0
Akbar Bobomurodov <dev.bobomurodov@gmail.com>
2025-06-02 00:19:20 +0500

    fix cors error

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://verifiedcarriers.com'], // frontend domain
+    credentials: true, // if you're using cookies or Authorization headers
+  });
+
   app.use((req, res, next) => {
     // console.log('Auth header:', req.headers.authorization);
     next();
   });
 
-  app.setGlobalPrefix('api/v1')
+  app.setGlobalPrefix('api/v1');
 
   // Enable CORS
   app.enableCors();
